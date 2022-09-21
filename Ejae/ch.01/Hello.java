43d5d84108601d1a32c0e59eb7a77ff10cb3f818
choiejae <ejae8319@gmail.com>
2022-09-21 16:12:30 +0900

    add ch.01

---
+++ Ejae/ch.01/Hello.java
@@ -0,0 +1,17 @@
+class class_name {
+	/*
+	 	 모든 code는 class 안에 존재한다
+	*/
+}
+
+// 소스파일의 이름은 public class 이름과 같아야 한다
+// 소스파일 내에 public class가 없다면 소스 파일의 이름은 어떠한 class 이름으로 하든 상관 없다
+// 하나의 소스파일에 두개의 public class가 있을 수 없다
+// public에 대해 7장에서 다룰 예정
+
+public class Hello {
+	public static void main(String[] args) { 
+	// main 메서드 선언. 프로그램 시
+		System.out.println("Hello, world");
+	}
+}
