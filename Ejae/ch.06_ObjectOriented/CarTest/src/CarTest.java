a26bec4842f7154065a5d10c9194616560b7e931
choiejae <ejae8319@gmail.com>
2022-10-03 20:38:59 +0900

    add: ch06 cartest

---
+++ Ejae/ch.06_ObjectOriented/CarTest/src/CarTest.java
@@ -0,0 +1,33 @@
+class Car {
+    String color;
+    String gearType;
+    int door;
+
+    Car() {
+        this("white", "auto", 4);
+        // Car(String color, String gearType, int door)를 호출
+    }
+
+    Car(String color) {
+        this(color, "auto", 4);
+    }
+
+    Car(String color, String gearType, int door) {
+        this.color = color;
+        this.gearType = gearType;
+        this.door = door;
+        
+    }
+}
+
+
+public class CarTest {
+    public static void main(String[] args) {
+
+        Car c1 = new Car();
+        Car c2 = new Car("blue");
+
+        System.out.println("c1의 Color = " + c1.color + " gearType = " + c1.gearType + " door = " + c1.door);
+        System.out.println("c2의 Color = " + c2.color + " gearType = " + c2.gearType + " door = " + c2.door);
+    }
+}
