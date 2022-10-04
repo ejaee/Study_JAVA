40ca53393642a431fcabc0395f90cf2ced25c295
Kang-SeoHyun <kang061020@gmail.com>
2022-10-04 21:52:29 +0900

    add: ch03

--- Seohyun/ch.03/Operator.java
+++ Seohyun/ch.03/Operator.java
@@ -1,5 +1,25 @@
 public class Operator {
     public static void main(String[] args) {
-        
+
+        int x = 0xAB;
+		int y = 0xF;
+		
+        // 비트 논리 연산자
+        System.out.println("-----------비트 논리 연산자-----------");
+		System.out.printf("x = %#X \t\t%s\n", x, Integer.toBinaryString(x));
+		System.out.printf("y = %#X \t\t%s\n", y, Integer.toBinaryString(y));
+	
+		System.out.printf("%#X | %#X = %#X \t%s\n",x, y, x|y, Integer.toBinaryString(x|y));
+		System.out.printf("%#X & %#X = %#X \t%s\n",x, y, x&y, Integer.toBinaryString(x&y));
+		System.out.printf("%#X ^ %#X = %#X \t%s\n",x, y, x^y, Integer.toBinaryString(x^y));
+
+        // 비트 이동 연산자
+        System.out.println("-----------비트 이동 연산자-----------");
+        System.out.printf("x = %#X \t\t%s\n", x, Integer.toBinaryString(x));
+        System.out.printf("x >>  4 %#X \t\t%s\n", x >> 32, Integer.toBinaryString(x >> 4));
+		System.out.printf("x <<  4 %#X \t\t%s\n", x << 32, Integer.toBinaryString(x << 4));
+		System.out.printf("x >> 32 %#X \t\t%s\n", x >> 32, Integer.toBinaryString(x >> 32));
+		System.out.printf("x << 32 %#X \t\t%s\n", x << 32, Integer.toBinaryString(x << 32));
+	
     }
 }
